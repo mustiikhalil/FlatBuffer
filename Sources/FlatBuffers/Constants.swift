744104c538a68fe5806854716ec7f25e5627ee27
mustii <mustii@mmk.one>
2019-09-18 18:05:22 +0300

    All the test cases for double without the little endian swap pass
    Test cases, and implementation to detect Endian
    
    Added support for endian conversion, adding doubles is no issue now
    
    Endian support
    
    Fixed a test case
    
    Clear memory after test is done
    
    Moved double init to bitpattern

--- Sources/FlatBuffers/Constants.swift
+++ Sources/FlatBuffers/Constants.swift
@@ -5,25 +5,47 @@ import Foundation
 
 private let isLitteEndian = CFByteOrderGetCurrent() == Int(CFByteOrderLittleEndian.rawValue)
 
-protocol Scaler: Equatable {}
+protocol Scaler: Equatable {
+    associatedtype NumaricValue
+    var convertedEndian: NumaricValue { get }
+}
 
-extension Scaler where Self: FixedWidthInteger {}
+extension Scaler where Self: FixedWidthInteger {
+    var convertedEndian: NumaricValue {
+        if isLitteEndian { return self as! Self.NumaricValue }
+        return self.littleEndian as! Self.NumaricValue
+    }
+}
 
-extension Double: Scaler {}
-extension Float: Scaler {}
+extension Double: Scaler {
+    typealias NumaricValue = UInt64
+    
+    var convertedEndian: UInt64 {
+        if isLitteEndian { return self.bitPattern }
+        return self.bitPattern.littleEndian
+    }
+}
+extension Float: Scaler {
+    typealias NumaricValue = UInt32
+    
+    var convertedEndian: UInt32 {
+        if isLitteEndian { return self.bitPattern }
+        return self.bitPattern.littleEndian
+    }
+}
 
-extension Int: Scaler {}
+extension Int: Scaler { typealias NumaricValue = Int }
 
-extension Int8: Scaler {}
-extension Int16: Scaler {}
-extension Int32: Scaler {}
-extension Int64: Scaler {}
+extension Int8: Scaler { typealias NumaricValue = Int8 }
+extension Int16: Scaler { typealias NumaricValue = Int16 }
+extension Int32: Scaler { typealias NumaricValue = Int32 }
+extension Int64: Scaler { typealias NumaricValue = Int64 }
 
-extension UInt8: Scaler {}
-extension UInt16: Scaler {}
+extension UInt8: Scaler { typealias NumaricValue = UInt8 }
+extension UInt16: Scaler { typealias NumaricValue = UInt16 }
 
-extension UInt32: Scaler {}
-extension UInt64: Scaler {}
+extension UInt32: Scaler { typealias NumaricValue = UInt32 }
+extension UInt64: Scaler { typealias NumaricValue = UInt64 }
 
 public typealias UOffset = UInt32
 public typealias SOffset = Int32
