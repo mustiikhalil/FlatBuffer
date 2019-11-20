c04ea5e76707aa639ca1f71a86c45261ec39963b
mustii <mustii@mmk.one>
2019-11-20 14:47:23 +0300

    Documentation for the mutatable protocol
    Removed platform

--- Sources/FlatBuffers/FlatBufferObject.swift
+++ Sources/FlatBuffers/FlatBufferObject.swift
@@ -1,20 +1,33 @@
 import Foundation
 
+/// FlatbufferObject structures all the Flatbuffers objects
 public protocol FlatBufferObject {
     init(_ bb: FlatBuffer, o: Int32)
 }
 
+/// Readable is structures all the Flatbuffers structs
+///
+/// Readable is a procotol that each Flatbuffer struct should confirm to since
+/// FlatBufferBuilder would require a Type to both create(struct:) and createVector(structs:) functions
 public protocol Readable: FlatBufferObject {
     static var size: Int { get }
     static var alignment: Int { get }
 }
 
+/// Mutable is a protocol that allows us to mutate Scalar values within the buffer
 public protocol Mutable {
+    /// makes Flatbuffer accessed within the Protocol
     var bb: FlatBuffer { get }
+    /// makes position of the table/struct  accessed within the Protocol
     var postion: Int32 { get }
 }
 
 extension Mutable {
+    
+    /// Mutates the memory in the buffer, this is only called from the access function of table and structs
+    /// - Parameters:
+    ///   - value: New value to be inserted to the buffer
+    ///   - index: index of the Element
     func mutate<T: Scalar>(value: T, o: Int32) -> Bool {
         guard o != 0 else { return false }
         bb.write(value: value, index: Int(o), direct: true)
@@ -24,7 +37,7 @@ extension Mutable {
 
 extension Mutable where Self == Table {
     
-    /// Mutates the value with respect to the position
+    /// Mutates a value by calling mutate with respect to the position in the table
     /// - Parameters:
     ///   - value: New value to be inserted to the buffer
     ///   - index: index of the Element
@@ -33,7 +46,9 @@ extension Mutable where Self == Table {
         return mutate(value: value, o: index + postion)
     }
     
-    /// Directly mutates the element at the index passed to it, this will ignore the position
+    /// Directly mutates the element by calling mutate
+    ///
+    /// Mutates the Element at index ignoring the current position by calling mutate
     /// - Parameters:
     ///   - value: New value to be inserted to the buffer
     ///   - index: index of the Element
@@ -44,7 +59,7 @@ extension Mutable where Self == Table {
 
 extension Mutable where Self == Struct {
     
-    /// Mutates the value with respect to the position
+    /// Mutates a value by calling mutate with respect to the position in the struct
     /// - Parameters:
     ///   - value: New value to be inserted to the buffer
     ///   - index: index of the Element
@@ -52,7 +67,9 @@ extension Mutable where Self == Struct {
         return mutate(value: value, o: index + postion)
     }
     
-    /// Directly mutates the element at the index passed to it, this will ignore the position
+    /// Directly mutates the element by calling mutate
+    ///
+    /// Mutates the Element at index ignoring the current position by calling mutate
     /// - Parameters:
     ///   - value: New value to be inserted to the buffer
     ///   - index: index of the Element
